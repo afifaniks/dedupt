7d7ffd101e77cc685c523961916527792b6b84cf
Afif Al Mamun <afifanik@gmail.com>
2025-01-09 14:13:27 -0700

    update formatter

---
+++ src/data/formatters.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class StackFormatter(ABC):
+    def __init__(self, from_top: bool, max_num_frames: int) -> None:
+        self.from_top = from_top
+        self.max_num_frames = max_num_frames
+
+    @abstractmethod
+    def format(self, stack: List[str]) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def name(self) -> str:
+        raise NotImplementedError
+
+
+class CppStackFormatter(StackFormatter):
+    def __init__(self, from_top: bool, max_num_frames: int) -> None:
+        super().__init__(from_top, max_num_frames)
+
+    def format(self, stack) -> str:
+        # Remove duplicate frames
+        stack = list(dict.fromkeys(stack))
+        stack = [frame for frame in stack if frame.lower() != "none"]
+        if self.max_num_frames > 0:
+            if self.from_top:
+                stack = stack[: self.max_num_frames]
+            else:
+                stack = stack[-self.max_num_frames :]
+
+        return "\n".join([f"{i+1}: {frame}" for i, frame in enumerate(stack)]).replace(
+            "_", " "
+        )
+
+    def name(self) -> str:
+        return "cpp"
+
+
+class JavaStackFormatter(StackFormatter):
+    def __init__(self, from_top: bool, max_num_frames: int) -> None:
+        super().__init__(from_top, max_num_frames)
+
+    def format(self, stack) -> str:
+        # Remove duplicate frames
+        stack = list(dict.fromkeys(stack))
+        stack = [frame for frame in stack if frame.lower() != "none"]
+        return "\n".join([f"{i+1}: {frame}" for i, frame in enumerate(stack)])
+
+    def name(self) -> str:
+        return "java"
+
+
+# Make a factory of fomatters
+def get_formatter(name: str, from_top: bool, max_num_frames: int) -> StackFormatter:
+    if name == "cpp":
+        return CppStackFormatter(from_top, max_num_frames)
+    elif name == "java":
+        return JavaStackFormatter(from_top, max_num_frames)
+    else:
+        raise ValueError(f"Unknown formatter name: {name}")
